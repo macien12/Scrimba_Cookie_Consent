2f58a1e1b543f9a692150bdaf0813d0081ec8d3b
macien12 <maciejnaw@pm.me>
2026-09-25 14:24:47 +0200

    Implement modal updates on consent submit
    Replace the placeholder/challenge comment with a working flow on form submit: insert loading HTML (renamed id uploadText -> upload-text), update the upload text after 1.5s to "Making the sale...", and after 3s replace the modal content with a final thank-you message and GIF. Removes the old challenge comment block and wires up the timed DOM updates for the modal.

--- index.js
+++ index.js
@@ -16,24 +16,21 @@ consentForm.addEventListener('submit', function(e){
     modalText.innerHTML = `
     <div class="modal-inner-loading">
         <img src="images/loading.svg" class="loading">
-        <p id="uploadText">
-            Uploading your data to the dark web...
-        </p>
-    </div>`
-/*
-Challenge:
-1. Take control of the "modal-text" element. 
-2. Make it so that when a user clicks on 
-   the accept button, the HTML string below
-   is inserted into the modal-text div.
-
-<div class="modal-inner-loading">
-    <img src="images/loading.svg" class="loading">
-    <p id="uploadText">
-        Uploading your data to the dark web...
-    </p>
-</div>
-*/ 
-})
- 
-
+        <p id="upload-text">Uploading your data to the dark web...</p>
+    </div>` 
+    
+    setTimeout(function(){
+        document.getElementById('upload-text').innerText = `
+        Making the sale...`
+    }, 1500)
+    
+    setTimeout(function(){
+        document.getElementById('modal-inner').innerHTML = `
+        <h2>Thanks you sucker! </h2>
+        <p>We just sold the rights to your eternal soul.</p>
+        <div class="idiot-gif">
+            <img src="images/pirate.gif">
+        </div>
+    ` 
+    }, 3000)
+}) 
