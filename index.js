a026fa82dc15da930659cf88d6e72df950527330
macien12 <maciejnaw@pm.me>
2026-09-25 19:45:34 +0200

    Added disabled close until submit

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ modalCloseBtn.addEventListener('click', function(){
 
 consentForm.addEventListener('submit', function(e){
     e.preventDefault()
+    
+    const consentFormData = new FormData(consentForm)
+    const fullName = consentFormData.get('fullName')
+    
     modalText.innerHTML = `
     <div class="modal-inner-loading">
         <img src="images/loading.svg" class="loading">
@@ -24,13 +28,22 @@ consentForm.addEventListener('submit', function(e){
         Making the sale...`
     }, 1500)
     
+/*   
+Challenge: 
+1. Make the button that closes the modal disabled.
+2. Make that button become usable when the final 
+   modal message has been displayed to the user.
+*/ 
+
     setTimeout(function(){
         document.getElementById('modal-inner').innerHTML = `
-        <h2>Thanks you sucker! </h2>
+        <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
         <p>We just sold the rights to your eternal soul.</p>
         <div class="idiot-gif">
             <img src="images/pirate.gif">
         </div>
-    ` 
+    `
+    modalCloseBtn.disabled = false
     }, 3000)
+  
 }) 
