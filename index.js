6e55a508f4276f39d6c81678937f928961fd402e
macien12 <maciejnaw@pm.me>
2026-09-25 13:41:57 +0200

    Added element when hitting submit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
+const consentForm = document.getElementById('consent-form')
+const modalText = document.getElementById('modal-text')
 
 setTimeout(function(){
     modal.style.display = 'inline'
@@ -8,4 +10,30 @@ setTimeout(function(){
 modalCloseBtn.addEventListener('click', function(){
     modal.style.display = 'none'
 })
+
+consentForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    modalText.innerHTML = `
+    <div class="modal-inner-loading">
+        <img src="images/loading.svg" class="loading">
+        <p id="uploadText">
+            Uploading your data to the dark web...
+        </p>
+    </div>`
+/*
+Challenge:
+1. Take control of the "modal-text" element. 
+2. Make it so that when a user clicks on 
+   the accept button, the HTML string below
+   is inserted into the modal-text div.
+
+<div class="modal-inner-loading">
+    <img src="images/loading.svg" class="loading">
+    <p id="uploadText">
+        Uploading your data to the dark web...
+    </p>
+</div>
+*/ 
+})
+ 
 
